d9699fa795fa2632a95212f28afe3803ddac8ffc
eujy <rxu.1094.9257@gmail.com>
2017-08-16 19:11:44 +0900

    pods tuika simasita

--- NR/NR/SelectViewController.swift
+++ NR/NR/SelectViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
+
+
 class SelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
