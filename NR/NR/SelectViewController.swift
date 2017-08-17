b456b937c1c245d78f4f317d200ed59bdbef0b5b
eujy <rxu.1094.9257@gmail.com>
2017-08-18 02:11:46 +0900

    emitが重なる

--- NR/NR/SelectViewController.swift
+++ NR/NR/SelectViewController.swift
@@ -10,32 +10,46 @@ import UIKit
 import Foundation
 import SocketIO
 
-let socket = SocketIOClient(socketURL: URL(string: "http://localhost:3000")!, config: [.forceWebsockets(true)])
-
-
 
 class SelectViewController: UIViewController {
-
+    
+    var socket: SocketIOClient!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        socket.connect()
-
-        // Do any additional setup after loading the view.
         
-        //nameとcharaNumをemit"rm_accessで送る
-        let name = "hoge"
+//        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+//        socket = appDelegate.socket
+       
         
-        socket.on("connect") { data, ack in
-            print("socket connected")
-            print("send message")
-            socket.emit("access", name)
-        }
-
-     
         
-
+//        var isConnected: Bool = false
+        
+//        socket.emit("in_socket","hoge")
+        
+//        if isConnected == false {
+//            DispatchQueue.main.asyncAfter(deadline: .now()+0.5){
+//                self.socket.connect()
+//            }
+//        }
+//        socket.connect()
+//
+//        
+//        
+//        
+//        // Do any additional setup after loading the view.
+//        
+//        //nameとcharaNumをemit"rm_accessで送る
+//       let name = "hoge"
+//        
+               
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -44,21 +58,37 @@ class SelectViewController: UIViewController {
     //MARK: Action
     
     @IBAction func ToBattle(_ sender: UIButton) {
+        
+//        socket.on("connect") { data, ack in
+//            print("socket connected")
+//            //print("send message")
+//            //          self.socket.emit("access", name)
+//            self.socket.emit("in_socket","hoge")
+//        }
+//        
+//        socket.on("in_ok") { data, ack in
+//            print("okkkkkkkkkkk")
+//            //print("send message2")
+//            // self.socket.emit("access", name)
+//            //            isConnected = true
+//        }
+        
+        
         let storyboard: UIStoryboard = UIStoryboard(name: "Battle", bundle: nil)
         let nextView = storyboard.instantiateViewController(withIdentifier: "Battle") as! BattleViewController
         self.present(nextView, animated: true, completion: nil)
     }
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
