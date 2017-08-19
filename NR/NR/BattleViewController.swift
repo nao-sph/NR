5f4f673171ccdce7c4d56df432364ee7d8f3b42e
eujy <rxu.1094.9257@gmail.com>
2017-08-19 21:27:52 +0900

    aiteni kougeki

--- NR/NR/BattleViewController.swift
+++ NR/NR/BattleViewController.swift
@@ -21,14 +21,21 @@ class BattleViewController : SelectViewController {
     @IBOutlet weak var MP2: UIProgressView!
     
     @IBAction func Damage(decrease: Float){
-        HP2.setProgress(HP2.progress - decrease, animated: true)
+        HP1.setProgress(HP1.progress - decrease, animated: true)
     }
     @IBAction func cure(increase: Float){
-        HP1.setProgress(HP1.progress + increase, animated: true)
+        HP2.setProgress(HP2.progress + increase, animated: true)
     }
     @IBAction func MPdamage(decrease: Float){
+        MP2.setProgress(MP2.progress - decrease, animated: true)
+    }
+    @IBAction func cureForLocal(increase: Float){
+        HP1.setProgress(HP1.progress + increase, animated: true)
+    }
+    @IBAction func MPdamageForLocal(decrease: Float){
         MP1.setProgress(MP1.progress - decrease, animated: true)
     }
+
     
     
     override func viewDidLoad() {
@@ -45,7 +52,7 @@ class BattleViewController : SelectViewController {
         
         
         //socket受信した時の処理
-        socket.on("to_everyone") { data, ack in
+        socket.on("to_room_without_self") { data, ack in
             
 //            let _data: NSDictionary = data[0] as! NSDictionary
 //            var str : NSString = _data[@"Key1"]
@@ -83,13 +90,17 @@ class BattleViewController : SelectViewController {
             case 2:
                 print("magiiiiiiiicccccccccccccccc")
                 self.Damage(decrease: 0.35)
+                self.MPdamage(decrease: 0.2)
                 break
             case 3:
                 print("barrrrrrrrrrrrrrrrrrrrierrrr")
-                self.Damage(decrease: 0.1)
+//                self.Damage(decrease: 0.1)
+                 self.Damage(decrease: -1)
                 break
             case 4:
                 print("chaaaaaaaaaarrrrrrrgeeeeeeee")
+                self.MPdamage(decrease: -0.25)
+                self.cure(increase: 0.2)
                 break
             default:
                 print("nothinggggggggggggggggggggg")
@@ -148,26 +159,24 @@ class BattleViewController : SelectViewController {
     
     @IBAction func attack(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
-        socket.emit("req_to_everyone",1)
+        socket.emit("req_to_room_without_self",1)
         
     }
     @IBAction func magic(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
-        socket.emit("req_to_everyone",2)
-        MPdamage(decrease: 0.2)
+        socket.emit("req_to_room_without_self",2)
         
     }
     @IBAction func barrier(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
-        socket.emit("req_to_everyone",3)
-        Damage(decrease: -1)
+        socket.emit("req_to_room_without_self",3)
     }
     
     @IBAction func charge(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
-        socket.emit("req_to_everyone",4)
-        MPdamage(decrease: -0.25)
-        cure(increase: 0.2)
+        socket.emit("req_to_room_without_self",4)
+        self.MPdamageForLocal(decrease: -0.25)
+        self.cureForLocal(increase: 0.2)
         
     }
     
