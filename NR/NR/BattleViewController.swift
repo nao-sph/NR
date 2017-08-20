6d81f690433ab0351402178ea38b498e42e3cdd9
eujy <rxu.1094.9257@gmail.com>
2017-08-20 18:27:08 +0900

    new method

--- NR/NR/BattleViewController.swift
+++ NR/NR/BattleViewController.swift
@@ -13,8 +13,9 @@ import Foundation
 
 class BattleViewController : SelectViewController {
     
-    //HP,MPの処理
     
+    //HP,MPの処理
+    let syohiMP: Float = 0.2
     @IBOutlet weak var HP1: UIProgressView!
     @IBOutlet weak var HP2: UIProgressView!
     @IBOutlet weak var MP1: UIProgressView!
@@ -26,30 +27,83 @@ class BattleViewController : SelectViewController {
     @IBAction func Damage2p(decrease: Float){
         HP2.setProgress(HP2.progress - decrease, animated: true)
     }
-    @IBAction func cure(increase: Float){
-        HP2.setProgress(HP2.progress + increase, animated: true)
-    }
-    @IBAction func MPdamage(decrease: Float){
+    @IBAction func MPdamage2p(decrease: Float){
         MP2.setProgress(MP2.progress - decrease, animated: true)
     }
-    @IBAction func MPdamageForLocal(decrease: Float){
+    @IBAction func MPdamage(decrease: Float){
         MP1.setProgress(MP1.progress - decrease, animated: true)
     }
     
-    
+    //8個のメソッド
+    var Seikou = true
+    @IBAction func A1(Seikou: Bool){
+        if(Seikou){
+            Damage2p(decrease: 0.25)
+        }
+    }
+    @IBAction func A2(Seikou: Bool){
+        if(Seikou){
+            Damage(decrease: 0.25)
+        }
+    }
+    @IBAction func M1(Seikou: Bool){
+        if(Seikou){
+            MPdamage(decrease: 0.35)
+            Damage2p(decrease: 0.35)
+        }else{
+            MPdamage(decrease: 0.35)
+            Damage(decrease: 0.35)
+        }
+    }
+    @IBAction func M2(Seikou: Bool){
+        if(Seikou){
+            MPdamage2p(decrease: 0.35)
+            Damage(decrease: 0.35)
+        }else{
+            MPdamage2p(decrease: 0.35)
+            Damage2p(decrease: 0.35)
+        }
+    }
+    @IBAction func C1(Seikou: Bool){
+        Damage(decrease: -0.2)
+        MPdamage(decrease: -0.25)
+    }
+    @IBAction func C2(Seikou: Bool){
+        Damage2p(decrease: -0.2)
+        MPdamage2p(decrease: -0.25)
+    }
+     func MyAction(Num: Int, OK: Bool ){
+        switch Num {
+        case 1:
+            A1(Seikou: OK)
+            break
+        case 2:
+            M1(Seikou: OK)
+            break
+        case 4:
+            C1(Seikou: OK)
+            break
+        default: break
+        }
+    }
+    func YourAction(Num: Int, OK: Bool ){
+        switch Num {
+        case 1:
+            A2(Seikou: OK)
+            break
+        case 2:
+            M2(Seikou: OK)
+            break
+        case 4:
+            C2(Seikou: OK)
+            break
+        default: break
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         var yourCmd: Int = 0
-        
-        // Do any additional setup after loading the view.
-        
-        //        socket.emit("in_socket","hoge")
-        //        socket.on("in_ok") { data, ack in
-        //            print("outtttttttttt")
-        //            //print("send message2")
-        //            // self.socket.emit("access", name)
-        //        }
         
         socket.on("turn_start"){data, ack in
             //startしてからの処理
@@ -72,42 +126,82 @@ class BattleViewController : SelectViewController {
                 print("aite" , yourCmd)
                 print("jibun", self.myCmd)
                 print("kekka",a)
-                if(a == 0 || a == 2){
-                    switch yourCmd {
-                    case 1:
-                        print("attackkkkkkkkkkkkkkkkkkkkk")
-                        self.Damage(decrease: 0.25)
-                        break
-                    case 2:
-                        print("magiiiiiiiicccccccccccccccc")
-                        self.Damage(decrease: 0.35)
-                        self.MPdamage(decrease: 0.2)
-                        break
-                    case 3:
-                        print("barrrrrrrrrrrrrrrrrrrrierrrr")
-                        //                self.Damage(decrease: 0.1)
-                        self.Damage(decrease: -1)
-                        break
-                    case 4:
-                        print("chaaaaaaaaaarrrrrrrgeeeeeeee")
-                        self.MPdamage(decrease: -0.25)
-                        self.cure(increase: 0.2)
-                        break
-                    default:
-                        print("nothinggggggggggggggggggggg")
-                    }
-                    self.buttonEnable()
-                    
+                
+                //新しい処理
+                if(a == 0){
+                    self.MyAction(Num: self.myCmd, OK: true)
+                    self.YourAction(Num: yourCmd, OK: true)
+                }else if(a == 1){
+                    self.MyAction(Num: self.myCmd, OK: true)
+                    self.YourAction(Num: yourCmd, OK: false)
+                }else if(a == 2){
+                    self.MyAction(Num: self.myCmd, OK: false)
+                    self.YourAction(Num: yourCmd, OK: true)
                 }
                 
-                self.buttonEnable()
-
+                //古い処理
                 
+                //                if(a == 0 || a == 2){
+                //                    switch yourCmd {
+                //                    case 1:
+                //                        print("attackされました")
+                //                        self.Damage(decrease: 0.25)
+                //                        break
+                //                    case 2:
+                //                        print("magic受けました")
+                //                        self.Damage(decrease: 0.35)
+                //                        self.MPdamage(decrease: 0.2)
+                //                        break
+                //                    case 3:
+                //                        print("barrierくらいました")
+                //                        //                self.Damage(decrease: 0.1)
+                //                        self.Damage(decrease: -1)
+                //                        break
+                //                    case 4:
+                //                        print("相手がchargeしました")
+                //                        self.MPdamage(decrease: -0.25)
+                //                        self.cure(increase: 0.2)
+                //                        break
+                //                    default:
+                //                        print("相手は何もしませんでした")
+                //                    }
+                //                    self.buttonEnable()
+                //
+                //                }
+                //                    switch self.myCmd {
+                //                    case 1:
+                //                        print("attackしました")
+                //                        self.Damage2p(decrease: 0.25)
+                //                        break
+                //                    case 2:
+                //                        print("magicしました")
+                //                        self.Damage2p(decrease: 0.35)
+                //                        self.MPdamageForLocal(decrease: self.syohiMP)
+                //                        break
+                //                    case 3:
+                //                        print("barrierしました")
+                //                        //                self.Damage(decrease: 0.1)
+                //                       self.cure(increase: 1)
+                //                        break
+                //                    case 4:
+                //                        print("自分はchargeしました")
+                //                        self.MPdamageForLocal(decrease: -0.25)
+                //                        self.Damage(decrease: -0.2)
+                //                        break
+                //                    default:
+                //                        print("自分は何もしませんでした")
+                //                    }
+                //                    self.buttonEnable()
+                //
+                //
+                //                self.buttonEnable()
+                //
+                //
             }
         }
         
         //socket受信した時の処理
-       
+        
         
     }
     
@@ -155,32 +249,25 @@ class BattleViewController : SelectViewController {
     @IBAction func attack(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
         socket.emit("req_to_room_without_self",1)
-        Damage2p(decrease: 0.25)
         myCmd = 1
         
     }
     @IBAction func magic(_ sender: UIButton) {
-        let syohiMP: Float = 0.2
         buttonBasics(BUTTON: sender)
         if(HP1.progress > syohiMP){
             socket.emit("req_to_room_without_self",2)
-            Damage2p(decrease: 0.35)
-            MPdamageForLocal(decrease: syohiMP)
             myCmd = 2
         }
     }
     @IBAction func barrier(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
         socket.emit("req_to_room_without_self",3)
-        cure(increase: 1)
         myCmd = 3
     }
     
     @IBAction func charge(_ sender: UIButton) {
         buttonBasics(BUTTON: sender)
         socket.emit("req_to_room_without_self",4)
-        self.MPdamageForLocal(decrease: -0.25)
-        self.Damage(decrease: -0.2)
         myCmd = 4
         
     }
